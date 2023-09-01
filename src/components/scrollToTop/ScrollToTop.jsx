63eb464157579ac3a7e8a155926d6ee130674fed
bartaka <90842048+bartaka@users.noreply.github.com>
2023-09-01 13:01:05 +0200

    implement scroll to top button, remove unused imports from AboutUs

---
+++ src/components/scrollToTop/ScrollToTop.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
+
+import styles from './ScrollToTop.module.scss';
+
+const ScrollToTop = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const goToTopHandler = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                setIsVisible(true);
+            } else setIsVisible(false);
+        });
+    }, []);
+
+    return (
+        <button
+            className={styles['scroll-to-top-button']}
+            style={{ display: isVisible ? 'block' : 'none' }}
+            onClick={goToTopHandler}
+        >
+            <FaArrowUp />
+        </button>
+    );
+};
+
+export default ScrollToTop;
