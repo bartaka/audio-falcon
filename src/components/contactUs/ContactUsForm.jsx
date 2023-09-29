2eb8a62d9b238769e323f57ab7c544bce85b614f
bartaka <90842048+bartaka@users.noreply.github.com>
2023-09-29 19:47:45 +0200

    portfo section touchup

--- src/components/contactUs/ContactUsForm.jsx
+++ src/components/contactUs/ContactUsForm.jsx
@@ -1,22 +1,21 @@
 import styles from './ContactUsForm.module.scss';
 
 const ContactUsForm = () => {
+  const handleSubmit = (e) => e.preventDefault();
 
-    const handleSubmit = (e) => e.preventDefault();
-
-    return (
-        <form className={styles['contact-us-form']} onSubmit={handleSubmit}>
-            <label htmlFor='name'>name</label>
-            <input type='text' id='name'></input>
-            <label htmlFor='email'>email</label>
-            <input type='email' id='email' required />
-            <label htmlFor='subject'>subject</label>
-            <input type='text' required></input>
-            <label htmlFor='message'>message</label>
-            <textarea id='message' required></textarea>
-            <button type='submit'>send</button>
-        </form>
-    );
+  return (
+    <form className={styles['contact-us-form']} onSubmit={handleSubmit}>
+      <label htmlFor='name'>name</label>
+      <input type='text' id='name'></input>
+      <label htmlFor='email'>email</label>
+      <input type='email' id='email' required />
+      <label htmlFor='subject'>subject</label>
+      <input type='text' required></input>
+      <label htmlFor='message'>message</label>
+      <textarea id='message' required></textarea>
+      <button type='submit'>send</button>
+    </form>
+  );
 };
 
 export default ContactUsForm;
