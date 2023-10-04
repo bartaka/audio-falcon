ed5691ef39a73769d6a4f3cba745a7fd704229db
bartaka <90842048+bartaka@users.noreply.github.com>
2023-10-04 22:48:19 +0200

    implement project overlays as portals

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <p>&copy; Audio Falcon {currentYear}</p>
       </div>
       <div>
-        <p>additional links</p>
+        <p>additional links?</p>
       </div>
     </footer>
   );
--- src/components/portfolio/ProjectTile.jsx
+++ src/components/portfolio/ProjectTile.jsx
@@ -1,32 +1,40 @@
 import { useState } from 'react';
 
 import styles from './ProjectTile.module.scss';
+import ProjectOverlay from './ProjectOverlay';
 
 import porftoPlaceholder from '../../assets/portfolio/portfo-placeholder.jpg';
 
 const ProjectTile = (props) => {
 
     const [tileOverlayed, setTileOverlayed] = useState(false);
+    const [overlayDisplayed, setOverlayDisplayed] = useState(false);
 
     const handleMouseOver = () => setTileOverlayed(true);
     const handleMouseOut = () => setTileOverlayed(false);
+    const handleTileClicked = () => setOverlayDisplayed(true);
+    const handleClose = () => setOverlayDisplayed(false);
 
     return (
-        <div
-            className={styles.tile}
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-        >
-            <img src={porftoPlaceholder} alt="" />
+        <>
+            {overlayDisplayed && <ProjectOverlay onClose={handleClose} />}
+            <div
+                className={styles.tile}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                onClick={handleTileClicked}
+            >
+                <img src={porftoPlaceholder} alt="" />
 
-            {tileOverlayed && (
-                <div className={styles.overlay}>
-                    <h4>{props.type}</h4>
-                    <p>{props.role}</p>
-                    <h3>{props.title}</h3>
-                </div>
-            )}
-        </div>
+                {tileOverlayed && (
+                    <div className={styles.overlay}>
+                        <h4>{props.type}</h4>
+                        <p>{props.role}</p>
+                        <h3>{props.title}</h3>
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
 
